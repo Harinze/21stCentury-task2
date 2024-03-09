5a0373c6131dcae215afecb097b7e5775ea92392
Kingsley Ibe <kingsley.ibe@decagon.dev>
2024-03-09 01:21:52 +0100

    added code to the repo

---
+++ pages/api/get-products.js
@@ -0,0 +1,46 @@
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+require('dotenv').config()
+
+const api = new WooCommerceRestApi({
+	url: process.env.STORE_URL ,
+	consumerKey: process.env.WC_CONSUMER_KEY,
+	consumerSecret: process.env.WC_CONSUMER_SECRET,
+	version: "wc/v3"
+});
+
+/**
+ * Get Products.
+ *
+ * Endpoint /api/get-products or '/api/get-products?perPage=2'
+ *
+ * @param req
+ * @param res
+ * @return {Promise<void>}
+ */
+export default async function handler(req, res) {
+	
+	const responseData = {
+		success: false,
+		products: []
+	}
+	
+	const { perPage } = req?.query ?? {};
+	
+	try {
+		const { data } = await api.get(
+			'products',
+			{
+				per_page: perPage || 50
+			}
+		);
+		
+		responseData.success = true;
+		responseData.products = data;
+		
+		res.json( responseData );
+		
+	} catch ( error ) {
+		responseData.error = error.message;
+		res.status( 500 ).json( responseData  );
+	}
+}
